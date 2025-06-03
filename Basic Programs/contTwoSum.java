a6dbb3452830492ba934bf0b347a460eb3f9da2b
Aswin13072004 <aswin1372004@gmail.com>
2025-06-03 20:23:38 +0530

    contTwoSum file

--- Basic Programs/contTwoSum.java
+++ Basic Programs/contTwoSum.java
@@ -1,18 +1,26 @@
 class contTwoSum{
     public static void main(String[] args) {
-        int [] array = {12, 31, 10, 6, 40};
+        int [] array = {2,3,11,4,5,8};
         int value = 0, largest = 0 ;
-        for(int i =0 ; i<array.length ; i++){
-            for(int j=i+1 ; j<array.length; j++){
-                System.out.println(i +" " + j);
-                value = array[i]+array[j];
-                System.out.println(value);
-                if (largest < value){
-                    largest = value;
-                }
-                break;
+        // for(int i =0 ; i<array.length ; i++){
+        //     for(int j=i+1 ; j<array.length; j++){
+        //         System.out.println(i +" " + j);
+        //         value = array[i]+array[j];
+        //         System.out.println(value);
+        //         if (largest < value){
+        //             largest = value;
+        //         }
+        //         break;
+        //     }
+        // }
+        // System.out.println("Largest = " + largest);
+        for(int i=0; i<array.length-1; i++){
+            value = array[i]+array[i+1];
+            System.out.println(value);
+            if(largest<value){
+                largest = value;
             }
         }
-        System.out.println("Largest = " + largest);
+        System.out.println("Largest"+largest);
     }
 }
